29d335815c5f774d44b2af61008e4e39c426465a
万万 <15863465252@163.com>
2021-12-14 21:46:06 +0800

    modify code

---
+++ Java_Basic/src/InputData/ScannerDemo.java
@@ -0,0 +1,24 @@
+package InputData;
+
+import java.util.Scanner;
+/*
+	数据输入：
+		导包：
+			import java.util.Scanner;
+		创建对象：
+			Scanner sc = new Scanner(System.in);
+		接收数据：
+			int x = sc.nextInt();
+*/
+public class ScannerDemo {
+	public static void main(String[] args) {
+		//创建对象
+		Scanner sc = new Scanner(System.in);
+
+		//接收数据
+		int x = sc.nextInt();
+
+		//输出数据
+		System.out.println("x:" + x);
+	}
+}
